5dd39d551d2e831b33dc62f58b4e5670dc0f3c59
bogeshwararao <bogeshwararao12042004@gmail.com>
2023-06-21 18:12:52 +0530

    commit

--- api/src/App.js
+++ api/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [users,setUsers]=useState([]);
+  const loadUsers=async ()=>{
+    const response=await fetch("https://api.github.com/users")
+    const jsonResponse=await
+    response.json();
+    setUsers(jsonResponse); 
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <center>
+        <div>
+          <h1>hello</h1>
+          <button onClick={loadUsers}>GET DATA</button>
+          <h2>USERS: </h2>
+          <ul>
+              {users.map(({id, login, avatar_url})=>(
+                <li key={id}>Name:{login}
+                <br></br>
+                Link:
+                {avatar_url}{" "}</li>
+                
+              ))}
+          </ul>
+        </div>
+        </center>
+
   );
 }
 
